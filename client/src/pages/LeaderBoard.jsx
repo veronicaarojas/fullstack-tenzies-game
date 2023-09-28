077c1b330b82d9cc0aa9ce9d083e6d98e6391f41
Veronica Rojas <ar_veronica@yahoo.com>
2023-09-28 18:10:35 +0100

    organize files

--- client/src/pages/LeaderBoard.jsx
+++ client/src/pages/LeaderBoard.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 
 function LeaderBoard() {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    
+
+  }, [records]);
+
+  const getRecords = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/leaderboard')
+    }
+  }
   return (
     <main>
       <div>
