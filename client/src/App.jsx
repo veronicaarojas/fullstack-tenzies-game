cdf9fac300750493be4e8bc4a57254dee1c2883a
Veronica Rojas <ar_veronica@yahoo.com>
2023-09-28 17:19:19 +0100

    client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,12 @@
 import './App.css';
-import Game from './pages /Game';
-import LeaderBoard from './pages /LeaderBoard';
+import Game from './pages/Game';
+import LeaderBoard from './pages/LeaderBoard';
 import NavBar from './components/NavBar';
 import { Routes, Route, Link } from 'react-router-dom';
 
 
 function App() {
   
-
-
-  
-
-  
-
-
-
-
   return (
     <div>
       <div>
