516b702ffe8eeae86d0087241ee469865096be4f
Veronica Rojas <ar_veronica@yahoo.com>
2023-10-09 19:04:24 +0100

    delete from the front end and hover buttons

--- client/src/pages/LeaderBoard.jsx
+++ client/src/pages/LeaderBoard.jsx
@@ -6,6 +6,21 @@ import { faTrashCan } from '@fortawesome/free-regular-svg-icons'
 function LeaderBoard() {
   const [records, setRecords] = useState([]);
   const reformattedData = reformatDates(records);
+  const [hoveredScores, setHoveredScores] = useState({});
+
+  const handleMouseOver = (RollID) => {
+    setHoveredScores((prevHoveredScores) => ({
+      ...prevHoveredScores,
+      [RollID]: true
+    }))
+  }
+
+  const handleMouseOut = (RollID) => {
+    setHoveredScores((prevHoveredScores) => ( {
+      ...prevHoveredScores, 
+      [RollID]: false
+    }))
+  }
   
 
   useEffect(() => {
@@ -48,6 +63,23 @@ function LeaderBoard() {
     return formattedDates;
   }
 
+  function handleDelete(RollID) {
+    deleteRecord(RollID)
+  }
+
+  const deleteRecord = async (RollID) => {
+    try {
+      const response = await fetch(`http://localhost:3001/delete-roll/${RollID}`, {
+        method: "DELETE"
+      });
+      if(!response.ok) console.log("an error occured")
+      getRecords();
+    } catch(err) {
+      console.log(err);
+    }
+
+  }
+
   return (
     <main>
       <div className='leaderboard--titles'>
@@ -63,7 +95,11 @@ function LeaderBoard() {
 
       
         {reformattedData.map((item) => 
-          <div key={item.RollID} className='leaderboard--item'> 
+          <div
+           key={item.RollID} 
+           className='leaderboard--item'
+           onMouseOver={() => handleMouseOver(item.RollID)}
+           onMouseOut={() => handleMouseOut(item.RollID)}> 
 
           <div className='leaderboard--date'>
           {item.Date}
@@ -71,10 +107,13 @@ function LeaderBoard() {
           <div>
           {item.Rolls}
           </div>
-
-          <button className='delete--button'>
+          {hoveredScores[item.RollID] === true && 
+          <button 
+          className='delete--button'
+          onClick={() => handleDelete(item.RollID)}>
           <FontAwesomeIcon icon={faTrashCan} />
           </button>
+          }
           
           
           </div>
